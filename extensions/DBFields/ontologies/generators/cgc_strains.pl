908d1cd72632bc9550cb997c24aa4c09f1c8752b
yostinso <yostinso>
2007-11-07 21:30:24 +0000

    Added generator and OBO for worm strains

--- extensions/DBFields/ontologies/generators/cgc_strains.pl
+++ extensions/DBFields/ontologies/generators/cgc_strains.pl
@@ -3,36 +3,45 @@
 use strict;
 
 use LWP::UserAgent;
+my $file = $ARGV[0];
 
-my $url = "https://dgrc.cgb.indiana.edu/cells/store/catalog.html";
+my $url = "http://biosci.umn.edu/CGC/strains/gophstrnt.txt";
 my $ua = new LWP::UserAgent();
-$ua->timeout(10);
+$ua->timeout(20);
 
+print STDERR "Downloading $url...\n";
 my $response = $ua->get($url);
+print STDERR "Parsing...\n";
 die "Couldn't get catalog page: $url because " . $response->message unless $response->is_success;
 my $content = $response->content . "\n";
 
-my $file = $ARGV[0];
+open CONT, '<', \$content or die "WFT";
 
-my ($catalog_table) = ($content =~ m/<div[^>]*>Cell Line Catalog<\/div>.*?<table[^>]*>((?:.(?!<\/table))*)/sm);
-my @rows = ($catalog_table =~ m/<tr>(.*?)<\/tr>/gsm);
-
-my $header_row = shift(@rows);
-my @headers = ($header_row =~ m/<th[^>]*>(.*?)<\/th>/gsm);
-
+my $current_strain = {};
 my @allvalues;
-foreach my $row (@rows) {
-  my @values = ($row =~ m/<td[^>]*>(.*?)<\/td>/gsm);
-  my %values_hash;
-  for (my $i = 0; $i < scalar(@values); $i++ ) {
-    $values[$i] =~ s/^\s*//;
-    $values[$i] =~ s/\s*$//;
-    $values[$i] =~ s/<[^>]*>//g;
-    $values[$i] =~ s/\s*\n+\s*/, /g;
-    $values_hash{$headers[$i]} = $values[$i];
+my $prevkey;
+while (my $line = <CONT>) {
+  $line =~ s/^[\r\n]+|[\r\n]+$//;
+  $line =~ s/^\s+|\s+$//;
+  if ($line =~ m/^[=-]+$/) {
+    if (length($current_strain->{'Strain'})) {
+      push(@allvalues, $current_strain);
+    }
+    $current_strain = {};
+    next;
   }
-  push(@allvalues, \%values_hash);
+  my ($key, $val) = ($line =~ m/^\s*(\S[^:]*):\s*(.*$)/);
+  if (length($key)) { 
+    $prevkey = $key; 
+  } else {
+    $val = $line;
+  }
+  if (length($prevkey)) {
+    $current_strain->{$prevkey} .= (!length($key)) ? ' ' : '';
+    $current_strain->{$prevkey} .= $val;
+  }
 }
+close CONT;
 
 
 my $mode;
@@ -53,13 +62,12 @@ my ($header_text) = ($content =~ m/((?:.(?!\[))*)/s);
 $content =~ s/\Q$header_text\E//;
 my @required_headers = (
   'format-version: 1.2', 
-  'remark: $Revision 1 $ Describes cell lines used in the modENCODE project',
-  'idspace: DGRC_INDIANA https://dgrc.cgb.indiana.edu/product/View?product=# "Indiana U. Dros. Genomics Resource Center Cell Line Catalog"',
+  'remark: $Revision 1 $ Describes worm strains used in the modENCODE project',
+  'idspace: CGC_STRAIN http://wormbase.org/db/gene/strain?class=Strain;name=# "Caenorhabditis Genetics Center Strain"',
 );
 foreach my $required_header (@required_headers) {
   if (!($header_text =~ m/^\Q$required_header\E/m)) { $header_text .= $required_header . "\n"; }
 }
-
 my @terms = ($content =~ m/(\[Term\](?:.(?!\[Term\]))*)/sg);
 my %parsed_terms;
 foreach my $term (@terms) {
@@ -72,22 +80,13 @@ foreach my $term (@terms) {
   }
   $parsed_terms{$term_hash{'id'} || $term_hash{'name'}} = \%term_hash;
 }
-#          {
-#            'Tissue Source' => 'hemocyte',
-#            'Species' => 'melanogaster',
-#            'Stock Number' => '147',
-#            'Special Features' => 'tumorous blood cells',
-#            'Cell Line' => 'mbn2'
-#          }
+
 foreach my $value (@allvalues) {
   my %newterm = (
-    'id'   => 'DGRC_INDIANA:' . $value->{'Stock Number'},
-    'name' => $value->{'Cell Line'},
-    'def'  => '',
+    'id'   => 'CGC_STRAIN:' . $value->{'Strain'},
+    'name' => $value->{'Strain'},
+    'def'  => $value->{'Description'},
   );
-  foreach my $var (keys(%$value)) { $newterm{'def'} .= "$var: " . $value->{$var} . '. ' if length($value->{$var}); }
-  chop($newterm{'def'});
-
   if (!defined($parsed_terms{$newterm{'id'}})) {
     $content .= "[Term]\n";
     foreach my $var (keys(%newterm)) {
@@ -95,16 +94,9 @@ foreach my $value (@allvalues) {
     }
     $content .= "\n";
   }
-  
 }
 
 $content = $header_text . $content;
 
 print $content;
-
-
-
-
-
-
 
