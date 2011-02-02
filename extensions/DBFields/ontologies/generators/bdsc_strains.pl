b1d469809ea79a8ea15447f37e33459007d9562b
yostinso <yostinso>
2011-02-02 00:13:33 +0000

    Fix strain.obo generators to be more consistent about updating existing OBOs

--- extensions/DBFields/ontologies/generators/bdsc_strains.pl
+++ extensions/DBFields/ontologies/generators/bdsc_strains.pl
@@ -3,103 +3,145 @@
 use strict;
 
 use Text::CSV;
+use File::Temp;
+use File::Copy;
 use LWP::UserAgent;
+
+sub contains {
+  my ($elem, @array) = @_;
+  foreach my $e (@array) {
+    return 1 if $e eq $elem;
+  }
+  return 0;
+}
+sub hash_equal {
+  my ($a, $b) = @_;
+  return 0 if length(keys(%$a)) != length(keys(%$b));
+  foreach my $k (keys(%$a)) {
+    return 0 unless exists($b->{$k});
+    return 0 if ($a->{$k} ne $b->{$k});
+  }
+  return 1;
+}
+
+sub parse {
+  my ($fh) = @_;
+  my @allvalues;
+  my $parser = new Text::CSV();
+  my $line = <$fh>; # Header line
+  $parser->parse($line);
+  my @header = $parser->fields();
+  while (my $line = <$fh>) {
+    $parser->parse($line);
+    my @fields = $parser->fields();
+    my %strain;
+    for (my $i = 0; $i < scalar(@header); $i++) {
+      $strain{$header[$i]} = $fields[$i];
+    }
+    push(@allvalues, \%strain);
+  }
+  return @allvalues;
+}
+
+
 my $file = $ARGV[0];
+my $content;
 
 my $url = "http://flystocks.bio.indiana.edu/bloomington.csv";
 my $ua = new LWP::UserAgent();
 $ua->timeout(20);
 
+my $cont;
+if (0) { # XXX
 print STDERR "Downloading $url...\n";
 my $response = $ua->get($url);
 print STDERR "Parsing...\n";
 die "Couldn't get catalog page: $url because " . $response->message unless $response->is_success;
-my $content = $response->content . "\n";
+$content = $response->content . "\n";
 
 
-open CONT, '<', \$content or die "WFT";
+open $cont, '<', \$content or die "WFT";
+} else { # XXX
+  open $cont, "<", "bloomington.csv" or die "oops";
+} # XXX
 
-my @allvalues;
-
-my $parser = new Text::CSV();
-
-my $line = <CONT>; # Header line
-$parser->parse($line);
-my @header = $parser->fields();
-while (my $line = <CONT>) {
-  $parser->parse($line);
-  my @fields = $parser->fields();
-  my %strain;
-  for (my $i = 0; $i < scalar(@header); $i++) {
-    $strain{$header[$i]} = $fields[$i];
-  }
-  push(@allvalues, \%strain);
-}
-close CONT;
+my @allvalues = parse($cont);
+close $cont;
 
 
-my $mode;
-if (! -e $file) { 
-  $mode = ">"; 
-} else {
-  $mode = "+<";
-}
-open FH, $mode, $file or die "Couldn't open $file for $mode";
-{
-  local $/;
-  $/ = undef;
-  $content = <FH>;
-}
-close FH;
 
-my ($header_text) = ($content =~ m/((?:.(?!\[))*)/s);
-$content =~ s/\Q$header_text\E//;
 my @required_headers = (
   'format-version: 1.2', 
   'remark: $Revision 1 $ Describes strains used in the modENCODE project',
   'idspace: BDSC_STRAIN http://flystocks.bio.indiana.edu/Reports/#.html "Bloomington Drosophila Stock Center at Indiana University"',
 );
-foreach my $required_header (@required_headers) {
-  if (!($header_text =~ m/^\Q$required_header\E/m)) { $header_text .= "\n" . $required_header ; }
-}
-my @terms = ($content =~ m/(\[Term\](?:.(?!\[Term\]))*)/sg);
-my %parsed_terms;
-foreach my $term (@terms) {
-  $term =~ s/\[Term\]//g;
-  $term =~ s/(^\s*)|(\s*$)//g;
-
-  my @matches = ($term =~ m/^([^:]*):\s*(.*)$/mg);
-  my %term_hash;
-  for (my $i = 0; $i < scalar(@matches); $i+=2) {
-    $term_hash{$matches[$i]} = $matches[$i+1];
+$content = "";
+my @headers;
+my %existing_terms;
+if (-e $file) {
+  open FH, "<", $file or die "Couldn't open $file for reading";
+  {
+    local $/;
+    $/ = undef;
+    $content = <FH>;
   }
-  $parsed_terms{$term_hash{'id'} || $term_hash{'name'}} = \%term_hash;
+  close FH;
+  my ($header_text) = ($content =~ /^([^\[]*)\[/s);
+  @headers = split(/(\r\n|\r|\n)/, $header_text);
+  @headers = grep { $_ !~ /^\s*$/ } @headers;
+  foreach my $rh (@required_headers) {
+    push @headers, $rh unless contains($rh, @headers);
+  }
+  my @terms = ($content =~ m/(\[Term\](?:.(?!\[Term\]))*)/sg);
+  foreach my $term (@terms) {
+    $term =~ s/\[Term\]//g;
+    $term =~ s/(^\s*)|(\s*$)//g;
+
+    my @matches = ($term =~ m/^([^:]*):\s*(.*)$/mg);
+    my %term_hash;
+    for (my $i = 0; $i < scalar(@matches); $i+=2) {
+      $term_hash{$matches[$i]} = $matches[$i+1];
+    }
+    $existing_terms{$term_hash{'id'}} = \%term_hash if $term_hash{'id'};
+  }
+} else {
+  @headers = @required_headers;
 }
 
 foreach my $value (@allvalues) {
+  next unless $value->{'Stk #'};
   my %newterm = (
     'id'   => 'BDSC_STRAIN:' . $value->{'Stk #'},
     'name' => $value->{'Genotype'},
     'def'  => $value->{'Comments'},
   );
-  if (!defined($parsed_terms{$newterm{'id'}})) {
-    $content .= "[Term]\n";
-    foreach my $var (keys(%newterm)) {
-      $newterm{$var} =~ s/\\/\\\\/g;
-      $newterm{$var} =~ s/!/\\!/g;
-      $newterm{$var} =~ s/\<.\>//g;
-      $newterm{$var} =~ s/\s+$//g; #trailing spaces
-      $newterm{$var} =~ s/"/\\"/g;
-      if ($var eq "def") {
-	$newterm{$var} = '"' . $newterm{$var} . '" [BBOP:modENCODE]';
-      }
-      $content .= "$var: " . $newterm{$var} . "\n";
-    }
-    $content .= "\n";
+  foreach my $var (keys(%newterm)) {
+    $newterm{$var} =~ s/\\/\\\\/g;
+    $newterm{$var} =~ s/!/\\!/g;
+    $newterm{$var} =~ s/\<.\>//g;
+    $newterm{$var} =~ s/\s+$//g; #trailing spaces
+    $newterm{$var} =~ s/"/\\"/g;
+  }
+  $newterm{"def"} = '"' . $newterm{"def"} . '" [BBOP:modENCODE]';
+  if (!defined($existing_terms{$newterm{'id'}}) || !hash_equal(\%newterm, $existing_terms{$newterm{'id'}})) {
+    $existing_terms{$newterm{'id'}} = \%newterm;
   }
 }
 
-$content = $header_text . "\n" . $content;
+my $out = new File::Temp( UNLINK => 1 );
+foreach my $header (@headers) {
+  print $out $header . "\n";
+}
+print $out "\n";
+foreach my $term (sort { $a->{'id'} cmp $b->{'id'} } values(%existing_terms)) {
+  print $out "[Term]\n";
+  foreach my $var (keys(%$term)) {
+    print $out "$var: " . $term->{$var} . "\n";
+  }
+  print $out "\n";
+}
+$out->flush;
 
-print $content;
-
+move($file, "$file.bak") or die "Couldn't create backup of $file" if (-e $file);
+copy($out->filename, $file) or die "Couldn't copy " . $out->filename . " to $file";
+close($out);
--- extensions/DBFields/ontologies/generators/cgc_strains.pl
+++ extensions/DBFields/ontologies/generators/cgc_strains.pl
@@ -2,107 +2,155 @@
 
 use strict;
 
+use File::Temp;
+use File::Copy;
 use LWP::UserAgent;
+
+sub contains {
+  my ($elem, @array) = @_;
+  foreach my $e (@array) {
+    return 1 if $e eq $elem;
+  }
+  return 0;
+}
+sub hash_equal {
+  my ($a, $b) = @_;
+  return 0 if length(keys(%$a)) != length(keys(%$b));
+  foreach my $k (keys(%$a)) {
+    return 0 unless exists($b->{$k});
+    return 0 if ($a->{$k} ne $b->{$k});
+  }
+  return 1;
+}
+
+sub parse {
+  my ($fh) = @_;
+  my $current_strain = {};
+  my @allvalues;
+  my $prevkey;
+  while (my $line = <$fh>) {
+    $line =~ s/^[\r\n]+|[\r\n]+$//;
+    $line =~ s/^\s+|\s+$//;
+    if ($line =~ m/^[=-]+$/) {
+      if (length($current_strain->{'Strain'})) {
+        push(@allvalues, $current_strain);
+      }
+      $current_strain = {};
+      next;
+    }
+    my ($key, $val) = ($line =~ m/^\s*(\S[^:]*):\s*(.*$)/);
+    if (length($key)) { 
+      $prevkey = $key; 
+    } else {
+      $val = $line;
+    }
+    if (length($prevkey)) {
+      $current_strain->{$prevkey} .= (!length($key)) ? ' ' : '';
+      $current_strain->{$prevkey} .= $val;
+    }
+  }
+  return @allvalues;
+}
+
+
 my $file = $ARGV[0];
+my $content;
 
 my $url = "http://biosci.umn.edu/CGC/strains/gophstrnt.txt";
 my $ua = new LWP::UserAgent();
 $ua->timeout(20);
 
+my $cont;
+if (0) { # XXX
 print STDERR "Downloading $url...\n";
 my $response = $ua->get($url);
 print STDERR "Parsing...\n";
 die "Couldn't get catalog page: $url because " . $response->message unless $response->is_success;
-my $content = $response->content . "\n";
-
-open CONT, '<', \$content or die "WFT";
-
-my $current_strain = {};
-my @allvalues;
-my $prevkey;
-while (my $line = <CONT>) {
-  $line =~ s/^[\r\n]+|[\r\n]+$//;
-  $line =~ s/^\s+|\s+$//;
-  if ($line =~ m/^[=-]+$/) {
-    if (length($current_strain->{'Strain'})) {
-      push(@allvalues, $current_strain);
-    }
-    $current_strain = {};
-    next;
-  }
-  my ($key, $val) = ($line =~ m/^\s*(\S[^:]*):\s*(.*$)/);
-  if (length($key)) { 
-    $prevkey = $key; 
-  } else {
-    $val = $line;
-  }
-  if (length($prevkey)) {
-    $current_strain->{$prevkey} .= (!length($key)) ? ' ' : '';
-    $current_strain->{$prevkey} .= $val;
-  }
-}
-close CONT;
+$content = $response->content . "\n";
 
 
-my $mode;
-if (! -e $file) { 
-  $mode = ">"; 
-} else {
-  $mode = "+<";
-}
-open FH, $mode, $file or die "Couldn't open $file for $mode";
-{
-  local $/;
-  $/ = undef;
-  $content = <FH>;
-}
-close FH;
+open $cont, '<', \$content or die "WFT";
+} else { # XXX
+  open $cont, "<", "gophstrnt.txt" or die "oops";
+} # XXX
 
-my ($header_text) = ($content =~ m/((?:.(?!\[))*)/s);
-$content =~ s/\Q$header_text\E//;
+my @allvalues = parse($cont);
+close $cont;
+
+
+
 my @required_headers = (
   'format-version: 1.2', 
   'remark: $Revision 1 $ Describes strains used in the modENCODE project',
   'idspace: CGC_STRAIN http://wormbase.org/db/gene/strain?class=Strain;name=# "Caenorhabditis Genetics Center Strain"',
 );
-foreach my $required_header (@required_headers) {
-  if (!($header_text =~ m/^\Q$required_header\E/m)) { $header_text .= "\n" . $required_header; }
-}
-my @terms = ($content =~ m/(\[Term\](?:.(?!\[Term\]))*)/sg);
-my %parsed_terms;
-foreach my $term (@terms) {
-  $term =~ s/\[Term\]//g;
-  $term =~ s/(^\s*)|(\s*$)//g;
-  my @matches = ($term =~ m/^([^:]*):\s*(.*)$/mg);
-  my %term_hash;
-  for (my $i = 0; $i < scalar(@matches); $i+=2) {
-    $term_hash{$matches[$i]} = $matches[$i+1];
+$content = "";
+my @headers;
+my %existing_terms;
+if (-e $file) {
+  open FH, "<", $file or die "Couldn't open $file for reading";
+  {
+    local $/;
+    $/ = undef;
+    $content = <FH>;
   }
-  $parsed_terms{$term_hash{'id'} || $term_hash{'name'}} = \%term_hash;
+  close FH;
+  my ($header_text) = ($content =~ /^([^\[]*)\[/s);
+  @headers = split(/(\r\n|\r|\n)/, $header_text);
+  @headers = grep { $_ !~ /^\s*$/ } @headers;
+  foreach my $rh (@required_headers) {
+    push @headers, $rh unless contains($rh, @headers);
+  }
+  my @terms = ($content =~ m/(\[Term\](?:.(?!\[Term\]))*)/sg);
+  foreach my $term (@terms) {
+    $term =~ s/\[Term\]//g;
+    $term =~ s/(^\s*)|(\s*$)//g;
+
+    my @matches = ($term =~ m/^([^:]*):\s*(.*)$/mg);
+    my %term_hash;
+    for (my $i = 0; $i < scalar(@matches); $i+=2) {
+      $term_hash{$matches[$i]} = $matches[$i+1];
+    }
+    $existing_terms{$term_hash{'id'}} = \%term_hash if $term_hash{'id'};
+  }
+} else {
+  @headers = @required_headers;
 }
 
 foreach my $value (@allvalues) {
+  next unless $value->{'Strain'};
   my %newterm = (
     'id'   => 'CGC_STRAIN:' . $value->{'Strain'},
     'name' => $value->{'Strain'},
     'def'  => $value->{'Description'},
   );
-  if (!defined($parsed_terms{$newterm{'id'}})) {
-    $content .= "[Term]\n";
-    foreach my $var (keys(%newterm)) {
-      $newterm{$var} =~ s/\\/\\\\/g;
-      $newterm{$var} =~ s/!/\\!/g;
-      $newterm{$var} =~ s/"/\\"/g;
-      if ($var eq "def") {
-	$newterm{$var} = '"' . $newterm{$var} . '" [BBOP:modENCODE]';
-      }
-      $content .= "$var: " . $newterm{$var} . "\n";
-    }
-    $content .= "\n";
+  foreach my $var (keys(%newterm)) {
+    $newterm{$var} =~ s/\\/\\\\/g;
+    $newterm{$var} =~ s/!/\\!/g;
+    $newterm{$var} =~ s/\<.\>//g;
+    $newterm{$var} =~ s/\s+$//g; #trailing spaces
+    $newterm{$var} =~ s/"/\\"/g;
+  }
+  $newterm{"def"} = '"' . $newterm{"def"} . '" [BBOP:modENCODE]';
+  if (!defined($existing_terms{$newterm{'id'}}) || !hash_equal(\%newterm, $existing_terms{$newterm{'id'}})) {
+    $existing_terms{$newterm{'id'}} = \%newterm;
   }
 }
 
-$content = $header_text . "\n" . $content;
+my $out = new File::Temp( UNLINK => 1 );
+foreach my $header (@headers) {
+  print $out $header . "\n";
+}
+print $out "\n";
+foreach my $term (sort { $a->{'id'} cmp $b->{'id'} } values(%existing_terms)) {
+  print $out "[Term]\n";
+  foreach my $var (keys(%$term)) {
+    print $out "$var: " . $term->{$var} . "\n";
+  }
+  print $out "\n";
+}
+$out->flush;
 
-print $content;
-
+move($file, "$file.bak") or die "Couldn't create backup of $file" if (-e $file);
+copy($out->filename, $file) or die "Couldn't copy " . $out->filename . " to $file";
+close($out);
