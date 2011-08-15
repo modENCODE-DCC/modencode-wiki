3d674bed41c749c813f06f780d8e43da7314a0ae
nlw <nlw>
2011-08-15 23:32:55 +0000

    updated conversion script for worm genes.  refseq ids are no longer available via biomart, so they have been replaced by genbank ids

--- extensions/DBFields/ontologies/generators/worm_genes.pl
+++ extensions/DBFields/ontologies/generators/worm_genes.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 
+#in order to run this script, you need a text file table of the genes dumped from wormmart.
+#instructions for doing this are as follows:
+#1.  go to wormbase.org.  click on WormMart
+#2.  select your build number data set.  the most recent version was WS220
+#3.  Select the following Attributes > Gene Annotation > Identification:
+#      Gene WB ID
+#      Gene Public Name
+#      Gene CGC Name
+#      Sequence Name (Gene)
+#      Gene Description (Concise)
+#      Source GenBank ID
+#4.  export to a txt file
+#5.  run this script like ./worm_genes.pl <infile> > <outfile>
+#    a timestamp will be added to the file
+
 use strict;
 
 use Date::Format;
@@ -10,7 +25,8 @@ open FH, $ARGV[0] or die "Couldn't open " . $ARGV[0];
 my @cols = split(/[\t\r\n]/, <FH>);
 use Data::Dumper;
 
-my @expected_cols = ("Gene WB ID", "Gene Public Name", "Gene CGC Name", "Sequence Name (Gene)", "NCBI RefSeq mRNA", "Gene Description (Concise)");
+#my @expected_cols = ("Gene WB ID", "Gene Public Name", "Gene CGC Name", "Sequence Name (Gene)", "NCBI RefSeq mRNA", "Gene Description (Concise)");
+my @expected_cols = ("Gene WB ID", "Gene Public Name", "Gene CGC Name", "Sequence Name (Gene)", "Gene Description (Concise)", "Source GenBank ID");
 foreach my $col (@expected_cols) {
   if (!in_array("Gene WB ID", @cols)) {
     die "Can't parse BioMart export without \"$col\" column.";
@@ -26,7 +42,7 @@ saved-by: yostinso
 auto-generated-by: worm_genes.pl
 synonymtypedef: cgc_name "Gene CGC Name" EXACT
 synonymtypedef: sequence_name "Sequence Name (Gene)" EXACT
-synonymtypedef: ncbi_refseq_mrna "NCBI RefSeq mRNA ID" EXACT
+synonymtypedef: ncbi_genbank_id "NCBI GenBank ID" EXACT
 default-namespace: worm_genes
 
 EOD
@@ -71,8 +87,11 @@ while (<FH>) {
   $synonym = "synonym: \"" . $cur_row{"Sequence Name (Gene)"} . "\" EXACT sequence_name []";
   push @{$cur_gene{"synonyms"}}, $synonym if $cur_row{"Sequence Name (Gene)"} && !in_array($synonym, $cur_gene{"synonyms"});
 
-  $synonym = "synonym: \"" . $cur_row{"NCBI RefSeq mRNA"} . "\" EXACT ncbi_refseq_mrna []";
-  push @{$cur_gene{"synonyms"}}, $synonym if $cur_row{"NCBI RefSeq mRNA"} && !in_array($synonym, $cur_gene{"synonyms"});
+  $synonym = "synonym: \"" . $cur_row{"Source GenBank ID"} . "\" EXACT ncbi_genbank_ID  []";
+  push @{$cur_gene{"synonyms"}}, $synonym if $cur_row{"Source GenBank ID"} && !in_array($synonym, $cur_gene{"synonyms"});
+
+#  $synonym = "synonym: \"" . $cur_row{"NCBI RefSeq mRNA"} . "\" EXACT ncbi_refseq_mrna []";
+#  push @{$cur_gene{"synonyms"}}, $synonym if $cur_row{"NCBI RefSeq mRNA"} && !in_array($synonym, $cur_gene{"synonyms"});
 }
 
 
